435467414c3f1c95f3c66696fbfa50bc6403e828
Mayuri Purohit <149376263+mayuuu05@users.noreply.github.com>
2024-08-24 18:53:23 +0530

    Add files via upload

---
+++ Qu5.java
@@ -0,0 +1,20 @@
+// WAP TO CREATE PATTERN AS MENTIONED BELOW IMAGE.
+
+public class Qu5 {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j < 5; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+
+            }
+            System.out.println();
+        }
+    }
+}
